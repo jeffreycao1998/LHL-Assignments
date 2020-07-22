cafd0bc9510a9205d1a81206ddbe0bce2ec51b95
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-07-22 15:24:21 -0400

    made flatten.js recursive

--- w1/lotide/flatten.js
+++ w1/lotide/flatten.js
@@ -13,19 +13,34 @@ const assertArraysEqual = (listA, listB) => {
   console.log(`🛑🛑🛑 Assertion Failed: ${listA} !== ${listB}`);
 };
 
+// NON-RECURSIVE
+
 // [1, 2, [3, 4], 5, [6]] => [1, 2, 3, 4, 5, 6]
+// const flatten = (array) => {
+//   let flattenedArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (Array.isArray(array[i])) {
+//       flattenedArray = flattenedArray.concat(array[i]);
+//     } else {
+//       flattenedArray.push(array[i])
+//     }
+//   }
+//   return flattenedArray;
+// };
+
+// RECURSIVE
 const flatten = (array) => {
   let flattenedArray = [];
 
-  for (let i = 0; i < array.length; i++) {
-    if (Array.isArray(array[i])) {
-      flattenedArray = flattenedArray.concat(array[i]);
+  for (let index of array) {
+    if (!Array.isArray(index)) {
+      flattenedArray.push(index);
     } else {
-      flattenedArray.push(array[i])
+      const flatArray = flatten(index);
+      flattenedArray = flattenedArray.concat(flatArray);
     }
   }
-
   return flattenedArray;
-};
+}
 
-console.log(flatten([1, 2, [[3, 3.5], 4], 5, [6]]));
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
