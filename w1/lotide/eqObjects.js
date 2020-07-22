057d0d4293bd56deac83c6ecdcfdc555f7b35dc1
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-07-21 21:02:09 -0400

    added eqObjects.js

---
+++ w1/lotide/eqObjects.js
@@ -0,0 +1,40 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const eqArrays = (listA, listB) => {
+  if (listA.length !== listB.length) return false;
+  for (let i = 0; i < listA.length; i++) {
+    if (listA[i] !== listB[i]) return false
+  }
+  return true;
+};
+
+const eqObjects = (objectA, objectB) => {
+  if (Object.keys(objectA).length !== Object.keys(objectB).length) return false;
+  
+  for (let property in objectA) {
+    if (Array.isArray(objectA[property] || Array.isArray(objectB[property]))) {
+      return eqArrays(objectA[property], objectB[property]);
+    } else if (objectA[property] !== objectB[property]) return false;
+  }
+  return true;
+};
+
+const ab = { a: '1', b: '2' };
+const ba = { b: '2', a: '1' };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: '1', b: '2', c: '3'};
+assertEqual(eqObjects(ab, abc), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
--- w1/lotide/findKeyByValue.js
+++ w1/lotide/findKeyByValue.js
@@ -7,9 +7,6 @@ const assertEqual = (actual, expected) => {
 };
 
 const findKeyByValue = (object, value) => {
-  // scan object
-  // return first key that contains value
-  // if no key found, return undefined
   for (let key in object) {
     if (object[key].toLowerCase().includes(value.toLowerCase())) {
       return key;
@@ -27,3 +24,4 @@ const bestTVShowsByGenre = {
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, "brooklyn nine-nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
