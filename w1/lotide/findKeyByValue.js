caa6aca0f3e1f4253a7b85c0f22e495bb21d62e4
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-07-21 20:36:27 -0400

    added findKeyByValue.js

---
+++ w1/lotide/findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const findKeyByValue = (object, value) => {
+  // scan object
+  // return first key that contains value
+  // if no key found, return undefined
+  for (let key in object) {
+    if (object[key].toLowerCase().includes(value.toLowerCase())) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "brooklyn nine-nine"), "comedy");
