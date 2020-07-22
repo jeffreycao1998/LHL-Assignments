5b69da10135e0733489bdfc9b9bfafb0e94cc68b
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-07-22 13:31:41 -0400

    finished map.js

---
+++ w1/lotide/map.js
@@ -0,0 +1,32 @@
+const eqArrays = (listA, listB) => {
+  for (let i = 0; i < listA.length; i++) {
+    if (listA[i] !== listB[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (listA, listB) => {
+  if (eqArrays(listA, listB)) {
+    console.log(`✅✅✅ Assertion Passed: ${listA} === ${listB}`);
+    return;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${listA} !== ${listB}`);
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = (array, callback) => {
+  const results = [];
+
+  array.forEach( item => {
+    results.push(callback(item));
+  });
+
+  return results;
+};
+
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(map(words, word => word.slice(1)), ['round', 'ontrol', 'o', 'ajor', 'om']);
+assertArraysEqual(map(words, word => word + 'XD'), ['groundXD', 'controlXD', 'toXD', 'majorXD', 'tomXD']);
