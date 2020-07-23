c9932cc6eb4253f64585ad33ed20b0661f980d0c
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-07-23 08:30:52 -0400

    updated eqArrays.js to check equality of nested arrays

--- w1/lotide/eqArrays.js
+++ w1/lotide/eqArrays.js
@@ -7,10 +7,26 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqArrays = (listA, listB) => {
+  if (listA.length !== listB.length) return false;
+
   for (let i = 0; i < listA.length; i++) {
-    if (listA[i] !== listB[i]) return false
+    if (Array.isArray(listA[i]) && Array.isArray(listB[i])) {
+      if (!eqArrays(listA[i], listB[i])) {
+        return false;
+      }
+    } else if (!Array.isArray(listA[i]) && !Array.isArray(listB[i])) {
+      if (listA[i] !== listB[i]) {
+        return false;
+      }
+    } else {
+      return false;
+    }
   }
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
