6cd257a39fccfa0d2defed6e35fa32623bdc03c3
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-07-23 08:13:43 -0400

    updated eqObjects to check equality of nested objects

--- w1/lotide/eqObjects.js
+++ w1/lotide/eqObjects.js
@@ -20,21 +20,30 @@ const eqObjects = (objectA, objectB) => {
   for (let property in objectA) {
     if (Array.isArray(objectA[property] || Array.isArray(objectB[property]))) {
       return eqArrays(objectA[property], objectB[property]);
-    } else if (objectA[property] !== objectB[property]) return false;
+    } else if (!Array.isArray(objectA[property] && typeof objectA[property] === 'object')) {
+      return eqObjects(objectA[property], objectB[property])
+    } else if (objectA[property] !== objectB[property]) {
+      return false;
+    }
   }
   return true;
 };
 
-const ab = { a: '1', b: '2' };
-const ba = { b: '2', a: '1' };
-assertEqual(eqObjects(ab, ba), true);
+// const ab = { a: '1', b: '2' };
+// const ba = { b: '2', a: '1' };
+// assertEqual(eqObjects(ab, ba), true);
 
-const abc = { a: '1', b: '2', c: '3'};
-assertEqual(eqObjects(ab, abc), false);
+// const abc = { a: '1', b: '2', c: '3'};
+// assertEqual(eqObjects(ab, abc), false);
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true);
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false);
+
+console.log(eqObjects({ a: { z: {p: 6 } }, b: 2 }, { a: { z: {p: 6 } }, b: 2 })); // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); //  => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+
