f9df27c25400aa4dbb646e413e7efc99187298b5
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-07-27 14:58:33 -0400

    deleted comments

--- w2/d2/spinner/spinner1.js
+++ w2/d2/spinner/spinner1.js
@@ -11,26 +11,21 @@ setTimeout(() => {
 }, 500);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
 }, 700);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r|   '); 
 }, 900);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r/   '); 
 }, 1100);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r-   '); 
 }, 1300);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r|   '); 
 }, 1500);
