c6931a33068ec8d4dddbb5295d514ca4a748dd47
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-07-27 09:22:18 -0400

    refactored tests to use mocha/chai in tail.js and middle.js

--- w1/lotide/test/middleTest.js
+++ w1/lotide/test/middleTest.js
@@ -1,7 +1,22 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const { assert } = require('chai');
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+describe("#middle", () => {
+  it("should return [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("should return [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("should return [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("should return [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+})
--- w1/lotide/test/tailTest.js
+++ w1/lotide/test/tailTest.js
@@ -1,10 +1,17 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const { assert } = require('chai');
 
-const words = ["Yes", "No", "Maybe"];
-
-tail(words);
-
-assertEqual(words.length, 3);
-assertEqual(tail(["yellow"]).length, 0);
-assertEqual(tail([]).length, 0);
+describe('#tail', () => {
+  it("should return 0 for ['yellow'].length", () => {
+    assert.strictEqual(tail(['yellow']).length, 0);
+  });
+  it("should return 2 for ['Yes', 'No', 'Maybe'].length", () => {
+    assert.strictEqual(tail(['Yes', 'No', 'Maybe']).length, 2);
+  });
+  it("should return ['No', 'Maybe'] for ['Yes', 'No', 'Maybe']", () => {
+    assert.deepEqual(tail(['Yes', 'No', 'Maybe']), ['No', 'Maybe']);
+  })
+  it("should return 0 for [].length", () => {
+    assert.strictEqual(tail([]).length, 0);
+  })
+})
