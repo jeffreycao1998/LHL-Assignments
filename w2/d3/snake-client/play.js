884e8459318bc2c65fbaaacb247bd2b23a88f856
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-07-28 09:21:46 -0400

    added keypress event handler

--- w2/d3/snake-client/play.js
+++ w2/d3/snake-client/play.js
@@ -1,5 +1,23 @@
 const { connect } = require('./client');
 
-console.log('Connecting...');
-connect();
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key)
+  });
+  return stdin;
+}
+
+console.log('Connecting...client');
+
+connect();
+setupInput();
