6c6284853557875d2138b524fff17aaca1aea7e4
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-07-28 06:16:37 -0400

    write data to connection on connect

--- w2/d3/snake-client/client.js
+++ w2/d3/snake-client/client.js
@@ -6,6 +6,12 @@ const connect = () => {
     host: '192.168.2.27',
     port: 50541,
   });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write("Name: JJC")
+  });
+
   conn.on('data', data => {
     console.log(data);
   });
