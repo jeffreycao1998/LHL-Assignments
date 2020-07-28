73cb06902cd13b5ae4bfd633a2d488bc34f95a5d
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-07-28 06:07:26 -0400

    cleared staging area

--- w2/d3/snake-client/play.js
+++ w2/d3/snake-client/play.js
@@ -1,20 +1,4 @@
-const net = require('net');
-
-// Establishes connection with game server
-const connect = () => {
-  const conn = net.createConnection({
-    host: '192.168.2.27',
-    port: 50541,
-  });
-  conn.on('data', data => {
-    console.log(data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  
-  return conn;
-};
+const { connect } = require('client');
 
 console.log('Connecting...');
 connect();
