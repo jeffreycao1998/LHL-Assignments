1b9a9f6fb7543844777de3abaae459daa2090c58
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-08-01 18:50:02 -0400

    added battleship pieces

--- battleship/script.js
+++ battleship/script.js
@@ -1,5 +1,48 @@
-const socket = io('http://localhost:3000');
 
-socket.on('connect', () => {
-  socket.emit('name', 'jeff');
+
+// const socket = io('http://localhost:8000');
+
+// socket.on('connect', () => {
+//   socket.emit('name', 'jeff');
+// });
+
+document.addEventListener('dblclick', () => {
+  document.documentElement.requestFullscreen().catch((err) => {
+    console.log(err);
+  });
 });
+
+const loadPlayerNames = (nameA, nameB) => {
+  $('#name-p1').text(nameA);
+  $('#name-p2').text(nameB);
+};
+
+// const loadPlayerPieces = ({carrier, battleship, cruiser, subarmine, destroyer}) => {
+//   const carrierHTML = '<div class=""'
+
+//   $('.all-pieces-p1').html('')
+// }
+
+// const loadPlayerSettings = ({
+//   shotsPerTurn, 
+//   boardSize, 
+//   carrier, 
+//   battleship, 
+//   cruiser, 
+//   submarine, 
+//   destroyer}) => {
+//     socket.emit('')
+// };
+
+const settings = {
+  shotsPerTurn: 1,
+  boardSize: 10,
+  carrier: 1,
+  battleship: 1,
+  cruiser: 1,
+  submarine: 1,
+  destroyer: 1
+}
+
+loadPlayerNames('Jim John', 'Pit Pat');
+// loadPlayerSettings(settings)
--- battleship/server.js
+++ battleship/server.js
@@ -1,32 +1,9 @@
-// const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
-
-// const app = require('express')();
-// const httpServer = require('http').createServer(app);
-
-// app.get('/', (req, res) => {
-//   res.send(content);
-// })
-
-// const io = require('socket.io')(httpServer);
+// const io = require('socket.io')(8000);
 
 // io.on('connect', socket => {
-//   console.log('connect');
-// });
+//   console.log(`${socket.id} has connected`);
 
-// io.on('name', data => {
-//   console.log(data);
+//   socket.on('name', data => {
+//     console.log(`hi ${data}`);
+//   })
 // })
-
-// httpServer.listen(3000, () => {
-//   console.log('go to http://localhost:3000');
-// });
-
-const io = require('socket.io')(3000);
-
-io.on('connect', socket => {
-  console.log(`${socket.id} has connected`);
-
-  socket.on('name', data => {
-    console.log(`hi ${data}`);
-  })
-})
