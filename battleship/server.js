c9ffa0b6e1e059ffe69f0ad52644ec4ced010736
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-08-01 21:57:27 -0400

    added css for board grid

--- battleship/server.js
+++ battleship/server.js
@@ -1,5 +1,15 @@
 // const io = require('socket.io')(8000);
 
+const p1 = {
+  name: 'Jim John',
+  ready: false,
+  
+}
+
+const p2 = {
+  name: 'Pit Pat'
+}
+
 // io.on('connect', socket => {
 //   console.log(`${socket.id} has connected`);
 
