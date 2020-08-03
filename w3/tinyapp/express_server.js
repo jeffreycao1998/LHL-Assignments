b65e11173363e75decb32566e3d57a9fd812627a
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-08-03 13:13:44 -0400

    added random string generator

--- w3/tinyapp/express_server.js
+++ w3/tinyapp/express_server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 8080;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const generateRandomString = () => {
+  const urlLetters = [
+    '1','2','3','4','5','6','7','8','9',
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+  ];
+  const result = [];
+  for (let i = 0; i < 6; i++) {
+    const randNum = (Math.round(Math.random() * 61));
+    result.push(urlLetters[randNum]);
+  }
+  console.log(result.join(''));
+  return result.join('');
+};
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set('view engine', 'ejs');
 
@@ -14,10 +34,19 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   let templateVars = { shortURL, longURL: urlDatabase[shortURL]};
   res.render('urls_show', templateVars)
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
 });
 
 
