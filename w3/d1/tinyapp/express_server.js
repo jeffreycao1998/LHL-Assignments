c3e660d4a33ea859ab1dbc43b7cc847ed49bcf41
Jeffrey Cao <jeffreycao1998@hotmail.com>
2020-08-03 07:48:23 -0400

    added ejs file

--- w3/d1/tinyapp/express_server.js
+++ w3/d1/tinyapp/express_server.js
@@ -9,17 +9,11 @@ const urlDatabase = {
 
 app.set('view engine', 'ejs');
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render('urls_index', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.send(`a = ${a}`)
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
