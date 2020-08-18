149ca0cc6bffcc30bcc05e3f3780c0535ce316cf
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-08-18 15:42:22 -0400

    Prevent sql injections with parameterized query

--- w5/BootcampX/bootcamp_app/students.js
+++ w5/BootcampX/bootcamp_app/students.js
@@ -11,13 +11,16 @@ const pool = new Pool({
   port: '1234'
 });
 
-pool.query(`
+const values = [`%${cohortName}%`, limit];
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as name
 FROM students
 JOIN cohorts ON cohort_id=cohorts.id
-WHERE cohorts.name LIKE '${cohortName}%'
-LIMIT ${Number(limit)};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
 })
--- w5/BootcampX/bootcamp_app/teachers.js
+++ w5/BootcampX/bootcamp_app/teachers.js
@@ -10,15 +10,18 @@ const pool = new Pool({
   port: '1234'
 });
 
-pool.query(`
+const values = [`%${cohortName}%`];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohortName}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   for (let query of res.rows) {
     console.log(`${query.cohort}: ${query.teacher}`);
