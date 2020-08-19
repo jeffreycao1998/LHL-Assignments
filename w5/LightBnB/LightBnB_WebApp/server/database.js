13789b1eae0096d70afcc41f889dc0f43172e91d
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-08-19 07:00:00 -0400

    implemented getAllReservations functionality

--- w5/LightBnB/LightBnB_WebApp/server/database.js
+++ w5/LightBnB/LightBnB_WebApp/server/database.js
@@ -22,10 +22,7 @@ const getUserWithEmail = function(email) {
   SELECT * FROM users
   WHERE email = $1
   `, [email])
-  .then(res => {
-    console.log(res.rows);
-    return res.rows[0];
-  });
+  .then(res => res.rows[0]);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -38,7 +35,7 @@ const getUserWithId = function(id) {
   return pool.query(`
   SELECT * FROM users
   WHERE id = $1`, [id])
-  .then(res => res.rows);
+  .then(res => res.rows[0]);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -68,7 +65,11 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT * FROM reservations
+  WHERE guest_id = $1
+  LIMIT $2`, [guest_id, limit])
+  .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
