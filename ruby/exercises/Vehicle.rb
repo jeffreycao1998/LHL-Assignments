e79235e5b57a8ab963b1a33afaf109d3bb670074
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-09-11 19:45:34 -0400

    ruby assignments

---
+++ ruby/exercises/Vehicle.rb
@@ -0,0 +1,99 @@
+module Towable
+  def can_tow?(pounds)
+    pounds < 2000 ? true : false
+  end
+end
+
+class Vehicle
+  attr_accessor :color
+  attr_reader :model, :year
+  @@number_of_vehicles = 0
+
+  def self.number_of_vehicles
+    puts "This program has created #{@@number_of_vehicles} vehicles"
+  end
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+    @@number_of_vehicles += 1
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallons of gas"
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "Your new #{color} paint job looks great!"
+  end
+
+  def age
+    "Your #{self.model} is #{years_old} years old."
+  end
+
+  private
+
+  def years_old
+    Time.now.year - self.year
+  end
+end
+
+class MyCar < Vehicle
+  include Towable
+
+  NUMBER_OF_DOORS = 4
+  
+  def to_s
+    "My car is a #{self.color}, #{self.year}, #{self.model}!"
+  end
+end
+
+class MyTruck < Vehicle
+  include Towable
+
+  NUMBER_OF_DOORS = 2
+
+  def to_s
+    "My truck is a #{self.color}, #{self.year}, #{self.model}!"
+  end
+end
+
+lumina = MyCar.new(1997, 'chevy lumina', 'white')
+lumina.speed_up(20)
+lumina.current_speed
+lumina.speed_up(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.shut_down
+MyCar.gas_mileage(13, 351)
+lumina.spray_paint("red")
+puts lumina
+puts MyCar.ancestors
+puts MyTruck.ancestors
+puts Vehicle.ancestors
+puts lumina.age
